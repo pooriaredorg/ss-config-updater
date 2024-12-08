e911566f6ec5cde122d908a072dd5dc903241cda
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2024-12-08 03:44:14 +0330

    fetch_configs.py

--- fetch_configs.py
+++ fetch_configs.py
@@ -8,7 +8,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 HEADERS = """//profile-title: base64:8J+RvUFub255bW91cyhQcm9qZWN0QWluaXRhKQ==
-//profile-update-interval: 24
+//profile-update-interval: 6
 //subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531
 //support-url: https://t.me/BXAMbot
 //profile-web-page-url: https://github.com/4n0nymou3"""
